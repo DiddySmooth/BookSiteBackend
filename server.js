ff3f08e160f5b76b24469616ff0dcce4835a84a9
avery.mcclead@gmail.com <avery.mcclead@gmail.com>
2023-05-01 15:19:34 -0500

    added files storage

--- server.js
+++ server.js
@@ -1,20 +1,42 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
+const fs = require('fs');
+
+
+let DataGrayson
+
+
+function getNewData(){
+    let rawDataGrayson = fs.readFileSync("graysonTierList.json")
+    DataGrayson = JSON.parse(rawDataGrayson)
+}
+app.use(cors())
+
+getNewData()
+
+var bookList = {
+        title: "The Name of the Wind",
+        author: "Patrick Rothfuss",
+        index: 1
+}
 
 const PORT = process.env.PORT || 8080
 
-app.get('/', function (req, res) {
+app.get('/',  function (req, res) {
   res.send('Hello World')
 })
 
 app.get('/grayson', function (req, res) {
-    var bookList = [
-        {
-            title: "The Name of the Wind",
-            author: "Patrick Rothfuss",
-            index: 1
-        }
-    ]
+    
+    console.log(DataGrayson)
+    res.send(DataGrayson)
 })
-
+app.post('/grayson/add', function (req, res) {
+    DataGrayson.push(bookList)
+    let data = JSON.stringify(DataGrayson)
+    fs.writeFileSync('graysonTierList.json', data)
+    getNewData()
+    res.send(DataGrayson)
+})
 app.listen(PORT)
